33c53464413842c1f92dc8635dd6b318393a296d
stellarhatchmilky <stellarhatchmilky@gmail.com>
2025-07-25 11:44:42 +0800

    init test project's infrastructure

---
+++ README.md
@@ -0,0 +1,4 @@
+## Setup
+1. Clone the repository and run `npm install --legacy-peer-deps` to install the dependencies.
+    > Please use Node version 18.20.8.
+2. Run `npm run start` to start the dev server.
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* code here */}
+      <h1>你好，这是React基础框架</h1>
+    </div>
+  );
+}
+
+export default App;
